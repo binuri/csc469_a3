d67d5d13117be277cedc72afda7146187d2af97c
Walpitagamage Binuri Mandula <g3tdirty@cdf.toronto.edu>
2016-04-07 04:10:03 -0400

    Connecting to location server

--- client_util.c
+++ client_util.c
@@ -29,109 +29,140 @@
 #ifdef USE_LOCN_SERVER
 static void build_req(char *buf)
 {
-	/* Write an HTTP GET request for the chatserver.txt file into buf */
+    /* Write an HTTP GET request for the chatserver.txt file into buf */
 
-	int nextpos;
+    int nextpos;
 
-	sprintf(buf,"GET /~csc469h/winter/chatserver.txt HTTP/1.0\r\n");
+    sprintf(buf,"GET /~csc469h/winter/chatserver.txt HTTP/1.0\r\n");
 
-	nextpos = strlen(buf);
-	sprintf(&buf[nextpos],"\r\n");
+    nextpos = strlen(buf);
+    sprintf(&buf[nextpos],"\r\n");
 }
 #endif /* USE_LOCN_SERVER */
 
 #ifdef USE_LOCN_SERVER
 static char *skip_http_headers(char *buf)
 {
-	/* Given a pointer to a buffer which contains an HTTP reply,
-	 * skip lines until we find a blank, and then return a pointer
-	 * to the start of the next line, which is the reply body.
-	 * 
-	 * DO NOT call this function if buf does not contain an HTTP
-	 * reply message.  The termination condition on the while loop 
-	 * is ill-defined for arbitrary character arrays, and may lead 
-	 * to bad things(TM). 
-	 *
-	 * Feel free to improve on this.
-	 */
+    /* Given a pointer to a buffer which contains an HTTP reply,
+     * skip lines until we find a blank, and then return a pointer
+     * to the start of the next line, which is the reply body.
+     * 
+     * DO NOT call this function if buf does not contain an HTTP
+     * reply message.  The termination condition on the while loop 
+     * is ill-defined for arbitrary character arrays, and may lead 
+     * to bad things(TM). 
+     *
+     * Feel free to improve on this.
+     */
 
-	char *curpos;
-	int n;
-	char line[256];
+    char *curpos;
+    int n;
+    char line[256];
 
-	curpos = buf;
+    curpos = buf;
 
-	while ( sscanf(curpos,"%256[^\n]%n",line,&n) > 0) {
-		if (strlen(line) == 1) { /* Just the \r was consumed */
-			/* Found our blank */
-			curpos += n+1; /* skip line and \n at end */
-			break;
-		}
-		curpos += n+1;
-	}
+    while ( sscanf(curpos,"%256[^\n]%n",line,&n) > 0) {
+        if (strlen(line) == 1) { /* Just the \r was consumed */
+            /* Found our blank */
+            curpos += n+1; /* skip line and \n at end */
+            break;
+        }
+        curpos += n+1;
+    }
 
-	return curpos;
+    return curpos;
 }
 #endif /* USE_LOCN_SERVER */
 
 #ifdef USE_LOCN_SERVER 
 int retrieve_chatserver_info(char *chatserver_name, u_int16_t *tcp_port, u_int16_t *udp_port)
 {
-	int locn_socket_fd;
-	char *buf;
-	int buflen;
-	int code;
-	int  n;
+    int locn_socket_fd;
+    char *buf;
+    int buflen;
+    int code;
+    int  n;
 
-	/* Initialize locnserver_addr. 
-	 * We use a text file at a web server for location info
-	 * so this is just contacting the CDF web server 
-	 */
+    /* Initialize locnserver_addr. 
+     * We use a text file at a web server for location info
+     * so this is just contacting the CDF web server 
+     */
 
-	/* 
-	 * 1. Set up TCP connection to web server "www.cdf.toronto.edu", 
-	 *    port 80 
-	 */
+    /* 
+     * 1. Set up TCP connection to web server "www.cdf.toronto.edu", 
+     *    port 80 
+     */
 
-	/**** YOUR CODE HERE ****/
+    /**** YOUR CODE HERE ****/
+     struct addrinfo hints;
+     struct addrinfo *locserv_addr_info;
 
-	/* The code you write should initialize locn_socket_fd so that
-	 * it is valid for the write() in the next step.
-	 */
+     memset(&hints, 0, sizeof(struct addrinfo));
+     hints.ai_family = AF_INET;
+     hints.ai_socktype = SOCK_STREAM;
 
-	/* 2. write HTTP GET request to socket */
+     if (getaddrinfo("www.cdf.toronto.edu", "http", &hints, &locserv_addr_info) < 0){
+        printf("ERROR: Failed to initialize valid address information from location server\n");
+        return -1;
+     }
 
-	buf = (char *)malloc(MAX_MSG_LEN);
-	bzero(buf, MAX_MSG_LEN);
-	build_req(buf);
-	buflen = strlen(buf);
+     if ((locn_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("ERROR: Error in Socket initialization for location server\n");
+        return -1;
+     }
 
-	write(locn_socket_fd, buf, buflen);
+     if(connect(locn_socket_fd, locserv_addr_info->ai_addr, locserv_addr_info->ai_addrlen)){
+        printf("ERROR: Failed to connect to the location server\n");
+        return -1;
+     }
 
-	/* 3. Read reply from web server */
+    /* The code you write should initialize locn_socket_fd so that
+     * it is valid for the write() in the next step.
+     */
 
-	read(locn_socket_fd, buf, MAX_MSG_LEN);
+    /* 2. write HTTP GET request to socket */
 
-	/* 
-	 * 4. Check if request succeeded.  If so, skip headers and initialize
-	 *    server parameters with body of message.  If not, print the 
-	 *    STATUS-CODE and STATUS-TEXT and return -1.
-	 */
+    buf = (char *)malloc(MAX_MSG_LEN);
+    bzero(buf, MAX_MSG_LEN);
+    build_req(buf);
+    buflen = strlen(buf);
 
-	/* Ignore version, read STATUS-CODE into variable 'code' , and record
-	 * the number of characters scanned from buf into variable 'n'
-	 */
-	sscanf(buf, "%*s %d%n", &code, &n);
+    write(locn_socket_fd, buf, buflen);
+
+    /* 3. Read reply from web server */
+
+    read(locn_socket_fd, buf, MAX_MSG_LEN);
+
+    /* 
+     * 4. Check if request succeeded.  If so, skip headers and initialize
+     *    server parameters with body of message.  If not, print the 
+     *    STATUS-CODE and STATUS-TEXT and return -1.
+     */
+
+    /* Ignore version, read STATUS-CODE into variable 'code' , and record
+     * the number of characters scanned from buf into variable 'n'
+     */
+    sscanf(buf, "%*s %d%n", &code, &n);
 
 
-	/**** YOUR CODE HERE ****/
+    /**** YOUR CODE HERE ****/
 
+    if (code != 200){
+        printf("ERROR: Request failed with code %d\n", code);
+    }
 
-	/* 5. Clean up after ourselves and return. */
+    char *reply_body = skip_http_headers(buf);
+    if(sscanf(reply_body, "%s %hu %hu", chatserver_name, tcp_port, udp_port) < 3){
+        printf("ERROR: Failed to scan the location server response.\n");
+        return -1;
+    } 
 
-	free(buf);
-	return 0;
+    /* 5. Clean up after ourselves and return. */
+
+    free(buf);
+    return 0;
 
 }
 
 #endif /* USE_LOCN_SERVER */
+
